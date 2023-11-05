8cee1ff5c65121ec27f36b25caad9e2e3bc8587a
stoitec <devunity69@gmail.com>
2023-11-05 22:35:45 +0100

    🔧 Update layout config

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      // GitHub: "",
+      // "Discord Community": "",
     },
   }),
 }
@@ -16,6 +16,15 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    Component.Graph({
+      localGraph: {
+        fontSize: 0.4, // what size should the node labels be?
+        opacityScale: 1.8,
+      },
+      globalGraph: {
+        fontSize: 0.4,
+      },
+    }),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
@@ -29,7 +38,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
